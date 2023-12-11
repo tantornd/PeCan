502dd32ba2daddf6e2953ab08569c7f57ed1e80c
Krit Hnoorun <krithnoorun@gmail.com>
2023-12-12 04:26:51 +0700

    nnn

---
+++ src/gameScene/CharacterPane.java
@@ -0,0 +1,11 @@
+package gameScene;
+
+import javafx.scene.layout.HBox;
+
+public class CharacterPane extends HBox {
+    public HBox 
+
+    public CharacterPane() {
+        setPrefHeight();
+    }
+}
